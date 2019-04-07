17caa87e68dd747d9bb01f30b3b586cf8aabd5d8
yck <>
2019-04-07 18:30:55 +0800

    feat: 解读 createElement

--- packages/react/src/ReactElement.js
+++ packages/react/src/ReactElement.js
@@ -53,9 +53,9 @@ function defineKeyPropWarningGetter(props, displayName) {
       warningWithoutStack(
         false,
         '%s: `key` is not a prop. Trying to access it will result ' +
-          'in `undefined` being returned. If you need to access the same ' +
-          'value within the child component, you should pass it as a different ' +
-          'prop. (https://fb.me/react-special-props)',
+        'in `undefined` being returned. If you need to access the same ' +
+        'value within the child component, you should pass it as a different ' +
+        'prop. (https://fb.me/react-special-props)',
         displayName,
       );
     }
@@ -74,9 +74,9 @@ function defineRefPropWarningGetter(props, displayName) {
       warningWithoutStack(
         false,
         '%s: `ref` is not a prop. Trying to access it will result ' +
-          'in `undefined` being returned. If you need to access the same ' +
-          'value within the child component, you should pass it as a different ' +
-          'prop. (https://fb.me/react-special-props)',
+        'in `undefined` being returned. If you need to access the same ' +
+        'value within the child component, you should pass it as a different ' +
+        'prop. (https://fb.me/react-special-props)',
         displayName,
       );
     }
@@ -108,6 +108,9 @@ function defineRefPropWarningGetter(props, displayName) {
  * @param {*} props
  * @internal
  */
+// 这就是个工厂函数，帮助我们创建 React Element 的
+// 内部代码很简单，无非多了一个 $$typeof 帮助我们标识
+// 这是一个 React Element
 const ReactElement = function(type, key, ref, self, source, owner, props) {
   const element = {
     // This tag allows us to uniquely identify this as a React Element
@@ -166,6 +169,7 @@ const ReactElement = function(type, key, ref, self, source, owner, props) {
 
 /**
  * Create and return a new ReactElement of the given type.
+ * 根据 type 返回一个新的 ReactElement
  * See https://reactjs.org/docs/react-api.html#createelement
  */
 export function createElement(type, config, children) {
@@ -178,18 +182,22 @@ export function createElement(type, config, children) {
   let ref = null;
   let self = null;
   let source = null;
-
+  // 判断是否传入配置，比如 <div className='11'></div> 中的 className 会被解析到配置中
   if (config != null) {
+    // 验证 ref 和 key，只在开发环境下
     if (hasValidRef(config)) {
       ref = config.ref;
     }
     if (hasValidKey(config)) {
       key = '' + config.key;
     }
-
+    // 赋值操作
+    // self 呢就是为了以后正确获取 this
+    // source 基本来说没啥用，内部有一些 filename, line number 这种
     self = config.__self === undefined ? null : config.__self;
     source = config.__source === undefined ? null : config.__source;
     // Remaining properties are added to a new props object
+    // 遍历配置，把内建的几个属性剔除后丢到 props 中
     for (propName in config) {
       if (
         hasOwnProperty.call(config, propName) &&
@@ -202,6 +210,7 @@ export function createElement(type, config, children) {
 
   // Children can be more than one argument, and those are transferred onto
   // the newly allocated props object.
+  // 处理 children 的几个操作，很简单
   const childrenLength = arguments.length - 2;
   if (childrenLength === 1) {
     props.children = children;
@@ -219,6 +228,8 @@ export function createElement(type, config, children) {
   }
 
   // Resolve default props
+  // 判断是否有给组件设置 defaultProps，有的话判断是否有给 props 赋值，只有当值为
+  // undefined 时，才会设置默认值
   if (type && type.defaultProps) {
     const defaultProps = type.defaultProps;
     for (propName in defaultProps) {
