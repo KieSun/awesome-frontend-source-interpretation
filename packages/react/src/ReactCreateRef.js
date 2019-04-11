2fba5f54710179d52a1f3d9ee5f213af297d32e1
Kiesun <zx597813039@gmail.com>
2019-04-11 17:47:10 +0800

    feat: createRef 解读

--- packages/react/src/ReactCreateRef.js
+++ packages/react/src/ReactCreateRef.js
@@ -9,6 +9,9 @@
 import type {RefObject} from 'shared/ReactTypes';
 
 // an immutable object with a single mutable value
+// 这个代码是不是贼简单，就是让你外部能够通过 current 去拿到 ref
+// 但是可能很多人没见过 Object.seal 这个 API
+// 直接给不清楚的各位复制了文档：封闭一个对象，阻止添加新属性并将所有现有属性标记为不可配置
 export function createRef(): RefObject {
   const refObject = {
     current: null,
